449b71f774e0bb19c65caf5bc5c31adb096987f8
Sankar Manoj <me@sankar-manoj.com>
2019-03-17 08:48:51 +0000

    Updated run_server script

--- Server/run_server.sh
+++ Server/run_server.sh
@@ -1,2 +1,4 @@
 cd /home/farm/project/FarmAutomation/Server && nohup python server.py &
 cd /home/farm/project/FarmAutomation/Server && nohup python pump_control.py &
+cd /home/farm/project/FarmAutomation/Server && nohup python remote-server.py &
+
